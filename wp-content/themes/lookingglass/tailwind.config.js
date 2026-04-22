a6c8bd37aaa0ba7635a1800b744b44e4b28f097e
Jose Aispuro beltran <aispuro@strangepixel.co>
2026-04-22 11:02:54 -0700

    Removed talwind.config.js from the gitignore

---
+++ wp-content/themes/lookingglass/tailwind.config.js
@@ -0,0 +1,52 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './assets/**/*.{ts,tsx,js,jsx}',
+        './**/*.php',
+        './blocks/**/*.php',
+        './views/**/*.{blade.php,php,html}',
+        './templates/**/*.{html,php}',
+        './template-parts/**/*.php',
+        './src/**/*.php',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    black: '#13140e',
+                    white: '#fbfbfb',
+                    red: '#f2695c',
+                    pink: '#e6a1d7',
+                    green: '#219292',
+                    yellow: '#f2c75b',
+                },
+            },
+            fontFamily: {
+                'arial-narrow': ['Arial Narrow', 'Arial', 'sans-serif'],
+                system: [
+                    '-apple-system',
+                    'BlinkMacSystemFont',
+                    '"Segoe UI"',
+                    'Roboto',
+                    'Oxygen-Sans',
+                    'Ubuntu',
+                    'Cantarell',
+                    '"Helvetica Neue"',
+                    'sans-serif',
+                ],
+            },
+            maxWidth: {
+                content: '620px',
+                wide: '1000px',
+            },
+            spacing: {
+                content: '620px',
+                wide: '1000px',
+            },
+            fontWeight: {
+                570: '570',
+            },
+        },
+    },
+    plugins: [],
+}
